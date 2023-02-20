170b79da28fb037e6b37e827573bb4ecaff5a431
ldk199662 <ldk199662@gmail.com>
2023-02-20 17:00:47 +0900

    myService

---
+++ src/main/java/com/example/app/service/MemberService.java
@@ -0,0 +1,64 @@
+package com.example.app.service;
+
+import com.example.app.domain.Member;
+import com.example.app.repository.MemberRepository;
+import com.example.app.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Transactional
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    //    회원가입
+    public Long join(Member member) {
+
+        long start = System.currentTimeMillis();
+
+       try {
+           validateDuplicateMember(member); // 중복 회원 검증
+
+           memberRepository.save(member);
+           return member.getId();
+       } finally {
+         long finish = System.currentTimeMillis();
+         long timeMs = finish - start;
+         System.out.println("join =" + timeMs + "ms");
+       }
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+        .ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    // 전체 회원 조회
+    public List<Member> findMembers() {
+        long start = System.currentTimeMillis();
+       try {
+           return memberRepository.findAll();
+       } finally {
+           long finish = System.currentTimeMillis();
+           long timeMs = finish - start;
+           System.out.println("findMembers =" + timeMs + "ms");
+       }
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
